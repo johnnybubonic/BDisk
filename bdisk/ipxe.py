7381cc3d39842ba7612e47c3f08ee3b5388a3d50
r00t <bts@square-r00t.net>
2016-11-29 03:16:44 -0500

    checking in... working on ipxe still

--- bdisk/ipxe.py
+++ bdisk/ipxe.py
@@ -1,8 +1,12 @@
 import os
 import shutil
 import jinja2
-import gitpython
+import git
+import patch
 
+
+def sslIPXE():
+    pass
 
 def buildIPXE(conf):
     build = conf['build']
@@ -13,5 +17,27 @@ def buildIPXE(conf):
     patches_dir = ipxe_tpl + '/patches'
     srcdir = build['srcdir']
     ipxe_src = srcdir + '/ipxe'
-    ipxe_git_uri = 
-    pass
+    ipxe_git_uri = 'git://git.ipxe.org/ipxe.git'
+    patches_git_uri = 'https://github.com/eworm-de/ipxe.git'
+    print('Now building iPXE in {0}. Please wait...'.format(ipxe_src))
+    # Get the source and apply some cherrypicks
+    if os.path.isdir(ipxe_src):
+        shutil.rmtree(ipxe_src)
+    ipxe_repo = git.Repo.clone_from(ipxe_git_uri, ipxe_src)
+    patches = ipxe_repo.create_remote('eworm', patches_git_uri)
+    patches.fetch()
+    eiso_commit = '189652b03032305a2db860e76fb58e81e3420c4d'
+    nopie_commit = '58557055e51b2587ad3843af58075de916e5399b'
+    # patch files
+    #cwd = os.getcwd()
+    #os.chdir(ipxe_src + '/src')
+    for p in ('01.git-version.patch.j2', '02.banner.patch.j2'):
+        try:
+            patch = fromfile(p)
+            patch.apply(strip = 2, root = ipxe_src + '/src')
+        except:
+            pass
+    #os.chdir(cwd)
+    # Patch using the files before applying the cherrypicks
+    ipxe_repo.git.cherry_pick('-n', eiso_commit)
+    ipxe_repo.git.cherry_pick('-n', nopie_commit)
---
+++ extra/templates/iPXE/patches/00-general.sed
@@ -0,0 +1,10 @@
+## Enable IPv6 support
+s/^#undef([[:space:]]*NET_PROTO_IPV6)/#define\1/g
+## Enable HTTPS
+s/^#undef([[:space:]]*DOWNLOAD_PROTO_HTTPS)/#define\1/g
+s@^//(#define[[:space:]]*IMAGE_TRUST_CMD@\1@g
+## Enable FTP
+s/^#undef([[:space:]]*DOWNLOAD_PROTO_FTP)/#define\1/g
+## Currently broken for EFI building
+#s@^//(#define[[:space:]]*CONSOLE_CMD)@\1@g
+#s@^//(#define[[:space:]]*IMAGE_PNG@\1@g
