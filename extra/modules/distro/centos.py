eec74f16e6963294498eb7159bb9685c3172c83e
r00t <bts@square-r00t.net>
2017-04-11 08:09:50 -0400

    config update, oops

---
+++ extra/modules/distro/centos.py
@@ -0,0 +1,11 @@
+distro = 'centos linux'
+pybin = '/usr/bin/python'
+script['pre'] = """#!/bin/bash
+touch /root/BDISK
+"""
+script['post'] = """#!/bin/bash
+rm -f /root/BDISK
+"""
+pkg['pre'] = ['yum', 'makecache']
+pkg['install'] = ['yum', '-y', 'install', '%PKG%']
+pkg['check'] = ['rpm', '-qi', '%PKG']
